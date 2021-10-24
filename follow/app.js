888bb95376f06f843e01454a1fea429f55d31312
jjangjae <nick2866@naver.com>
2021-10-24 13:58:55 +0900

    follow 서버 셋팅

--- follow/app.js
+++ follow/app.js
@@ -1,41 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const app = require("./server");
+const logger = require("./config/logger");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.EXPRESS_PORT;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//test용 시작 view page
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get("/", (req, res) => {
+  res.render("index");
 });
+//test용 끝
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.listen(port, () => {
+  logger.info(`${port} 포트에서 서버가 가동되었습니다.`);
 });
-
-module.exports = app;
---
+++ follow/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const app = express();
+//react 연결지워야할 것
+// app.use("/", (req, res) => {
+//     return res.send(express.static(path.join(__dirname, "client/index.html")));
+//   });
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+//routing
+// app.use("/api");
+
+//Error handler
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
